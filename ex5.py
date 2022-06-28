426a1e5840f3ce42df9f88edcf3224f5f1d42b15
Yonatan Reinstein <ryonatan@campus.technion.ac.il>
2022-06-28 19:56:49 +0300

    tests

--- ex5.py
+++ ex5.py
@@ -11,7 +11,9 @@ def names_of_registered_students(input_json_path, course_name):
     :param course_name: The name of the course.
     :return: List of the names of the students.
     """
-    pass
+    with open(input_json_path, 'r') as input_file:
+        student_dict = json.load(input_file)
+        return [ student["student_name"] for student in student_dict.values() if course_name in student["registered_courses"]]
 
 
 def enrollment_numbers(input_json_path, output_file_path):
@@ -21,7 +23,22 @@ def enrollment_numbers(input_json_path, output_file_path):
     :param input_json_path: Path of the students database json file.
     :param output_file_path: Path of the output text file.
     """
-    pass
+    with open(input_json_path, 'r') as json_file:
+        main_input_dict = json.load(json_file)
+        main_output_list = []
+        for student in main_input_dict.values():
+            for course in student["registered_courses"]:
+                if(course in main_output_list):
+                    continue
+                else:
+                    main_output_list.append(course)
+        main_output_list.sort()
+    with open(output_file_path, 'w') as file:
+        print(main_output_list)
+        for course in main_output_list:
+            file.write('"' + course + '" ' +  str(len(names_of_registered_students(input_json_path, course))) + "\n")
+
+
 
 
 
@@ -31,4 +48,20 @@ def courses_for_lecturers(json_directory_path, output_json_path):
     :param json_directory_path: Path of the semsters_data files.
     :param output_json_path: Path of the output json file.
     """
-    pass
+    output_dict = {}
+    for input_file in os.listdir(json_directory_path):
+        name, ext = os.path.splitext(input_file)
+        if ext == '.json':
+            file_path = os.path.join(json_directory_path, input_file)
+            print (file_path)
+            with open(file_path, 'r') as json_file:
+                main_input_dict = json.load(json_file)
+                for course_dict in main_input_dict.values():
+                    lecturers = course_dict.get('lecturers')
+                    for lecturer in lecturers:
+                        output_dict.setdefault(lecturer, [])
+                        course_name = course_dict["course_name"]
+                        output_dict[lecturer].append(course_name) if course_name\
+                             not in output_dict[lecturer] else output_dict[lecturer]
+    with open(output_json_path, 'w') as file:
+        json.dump(output_dict, file, indent=4)
--- test.py
+++ test.py
@@ -8,43 +8,43 @@ database_directory_path = "semesters_databases"
 class Test(unittest.TestCase):
 
 
-    # def test1(self):
-    #     """
-    #     this test for the first function - names_of_registered_students.
-    #     """
+     def test1(self):
+         """
+         this test for the first function - names_of_registered_students.
+         """
 
-    #     correct_answer = ["Mohamed Gramne", "Mhameed", "Efrat Levkovizh", "Yazeed Falah"]
+         correct_answer = ["Mohamed Gramne", "Mhameed", "Efrat Levkovizh", "Yazeed Falah"]
 
-    #     result = names_of_registered_students(input_json_path,"Introduction to Systems Programming")
+         result = names_of_registered_students(input_json_path,"Introduction to Systems Programming")
 
-    #     correct_answer.sort()
-    #     result.sort()
+         correct_answer.sort()
+         result.sort()
 
-    #     self.assertListEqual(correct_answer, result, "Error: the output of the function   \
-    #                                               names_of_registered_students        \
-    #                                               doesn't match the expected one.")
+         self.assertListEqual(correct_answer, result, "Error: the output of the function   \
+                                                   names_of_registered_students        \
+                                                   doesn't match the expected one.")
 
-    # def test2(self):
-    #     """
-    #     this test for the second function - enrollment_number.
-    #     """
+     def test2(self):
+         """
+         this test for the second function - enrollment_number.
+         """
 
-    #     expected_output_file = "expected_output_test2_enrollment_numbers.txt"
-    #     enrollment_numbers(input_json_path,"test2.out")
-    #     with open("test2.out") as file_to_test:
-    #         data_to_test = list(file_to_test)
+         expected_output_file = "expected_output_test2_enrollment_numbers.txt"
+         enrollment_numbers(input_json_path,"test2.out")
+         with open("test2.out") as file_to_test:
+             data_to_test = list(file_to_test)
 
-    #     with open(expected_output_file) as expected_file:
-    #         expected_data = list(expected_file)
+         with open(expected_output_file) as expected_file:
+             expected_data = list(expected_file)
 
-    #     self.assertListEqual(data_to_test,expected_data, "Error: the content of the file created by   \
-    #                                           the fucntion - enrollment_number     \
-    #                                           doesn't match the expected one.")
+         self.assertListEqual(data_to_test,expected_data, "Error: the content of the file created by   \
+                                               the fucntion - enrollment_number     \
+                                               doesn't match the expected one.")
 		
 		
 		
 
-    def test3(self):
+     def test3(self):
         
         """
         this test for the third function - courses_for_lecturers.
